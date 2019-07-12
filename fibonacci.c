ef6e4b34d17f2e71e0901fb5c07193feeb78a849
suneetasharvi <suneetagdevops@gmail.com>
2019-07-12 13:19:44 +0530

    create fibonacci

---
+++ fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void fibonacci()
+{
+   int f1=0, f2=1, fib_ser, cnt=2, lmt;
+ 
+   printf("Please enter the limit of the Fibonacci series :");
+   scanf("%d",&lmt);
+   printf("\nFibonacci series is: \n%d \n%d \n",f1,f2);
+ 
+   while (cnt < lmt)
+   {
+      fib_ser=f1+f2;
+      cnt++;
+      printf("%d\n",fib_ser);
+      f1=f2;
+      f2=fib_ser;
+   }
+   
+}
